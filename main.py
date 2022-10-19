8545a8fc10dc5bb12fc4c86cb1da25c4defed5fe
rishic3 <rchand18@jhu.edu>
2022-10-18 22:13:54 -0400

    Tracker and max ROI object detection

--- main.py
+++ main.py
@@ -2,22 +2,55 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-import flask
-import deeplake
-from torchvision import datasets, transforms, models
+import cv2
+from tracker import *
 
-lspTrain = deeplake.load("hub://activeloop/lsp-train")
-lspTest = deeplake.load("hub://activeloop/lsp-test")
+cap = cv2.VideoCapture('images/rishiSquat2.mp4')
 
-print(lspTest.summary())
+# Create tracker object
+tracker = EuclideanDistTracker()
 
-tform = transforms.Compose([
-    transforms.Resize(size=[130, 130]),
-    transforms.ToTensor(), # Must convert to pytorch tensor for subsequent operations to run
-])
+# Object detection from Stable camera
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
-dataloader = lspTrain.pytorch(num_workers=0, batch_size=4, shuffle=False, transform = {'images': tform})
+while True:
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
 
-for data in dataloader:
-    print(data)
-    break
+    # Extract Region of interest
+    roi = frame
+
+    # 1. Object Detection
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    areas = {}
+    maxArea = 0
+    maxCnt = None
+    for cnt in contours:
+        # Calculate area and remove small elements
+        if cv2.contourArea(cnt) > maxArea:
+            maxArea = cv2.contourArea(cnt)
+            maxCnt = cnt
+
+    x, y, w, h = cv2.boundingRect(maxCnt)
+    detections.append([x, y, w, h])
+
+    # 2. Object Tracking
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
